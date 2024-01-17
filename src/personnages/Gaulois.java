a208a3736238e7f50f78960c6e49df5038925c75
CypCypC <cyprien.xwb@gmail.com>
2024-01-17 09:10:13 +0100

    question 5

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,14 +3,23 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	
-	public String getNom() {
-		return nom;
-	}
-	
+
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
+
+	public String getNom() {
+		return nom;
+	}
 	
+	public int getForce() {
+		return force;
+	}
+	
+	
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom()); 
+	}
 }
